50505443e0d77df807f0ae3c11601bb2ef89539d
Priya Kumari <chaudhary21priya@gmail.com>
2025-09-05 12:54:12 +0530

    feat: setup page routes

---
+++ src/fonts/calsans.ts
@@ -0,0 +1,7 @@
+import localFont from "next/font/local";
+
+export const calsans = localFont({
+  src: "./CalSans-SemiBold.woff2", // path to your font file
+  variable: "--font-calsans",
+  display: "swap",
+});
